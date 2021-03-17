b9cfd44b430f1140fd09274b25f21232be8cae1b
Giovanni <giovannipor23@gmail.com>
2021-03-17 19:04:16 +0100

    Milestone 2

--- js/tmp.js
+++ js/tmp.js
@@ -98,20 +98,43 @@ $(document).ready(function () {
 		}
 	];
 
+const COLORS = ['red', 'green', 'blue'];
 const list = $('#icons');
-print(icons,list);
-
-/* icons.forEach(icon => {
-	const {name, prefix, type, family} = icon;
-	$('#icons').append(`<li><i class="${family} ${prefix}${name} ${type}"></i>${name}</li>`)
-	console.log(prefix, family,name, type);
-}); */
+const COLORARRAY = colors(icons, COLORS);
+console.log(COLORARRAY);
+print(COLORARRAY,list);
 });
 
 function print(array, list) {
-	array.forEach(item => {
-		const {color, name, family, prefix} = item;
-		const HTMLel = `<li><i class="${family} ${prefix}${name}"></i>${name}</li>`;
+	array.forEach(ICON => {
+		const {color, name, family, prefix} = ICON;
+		const HTMLel = `<li><i class="${family} ${prefix}${name} ${color}"></i> ${name} </li>`;
 		list.append(HTMLel);
 	});
+};
+
+
+function colors(icons, COLORS) {
+	const TYPES = getType(icons);
+	const COLORARRAY = [];
+	for(let i = 0; i < icons.length; i++) {
+		const ICON = icons[i];
+		const IDXTYPE = TYPES.indexOf(ICON.type);
+		if(IDXTYPE !== -1) {
+			ICON.color = COLORS[IDXTYPE];
+		};
+		COLORARRAY.push(ICON);
+	};
+	return COLORARRAY;
+};
+
+
+function getType(array) {
+	const TYPES = [];
+	array.forEach(icon => {
+		if(TYPES.includes(icon.type) == false) {
+			TYPES.push(icon.type);
+		};
+	});
+	return TYPES;
 };
